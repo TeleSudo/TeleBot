a9efdf584687200a0e008b29a247ec976a48832e
Abolfazl Fallah <fallahabolfazl2@yahoo.com>
2016-09-26 22:41:44 +0330

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,13 +5,87 @@ cd $THIS_DIR
 
 update() {
   git pull
-  git submodule update --init --recursive
+  git submodule update —init —recursive
+  install_rocks
+  in_tg
+}
+
+# Will install luarocks on THIS_DIR/.luarocks
+install_luarocks() {
+  git clone https://github.com/keplerproject/luarocks.git
+  cd luarocks
+  git checkout tags/v2.2.1 # Current stable
+
+  PREFIX="$THIS_DIR/.luarocks"
+
+  ./configure —prefix=$PREFIX —sysconfdir=$PREFIX/luarocks —force-config
+
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  make build && make install
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting.";exit $RET;
+  fi
+
+  cd ..
+  rm -rf luarocks
+}
+
+in_tg() {
+  git clone https://github.com/SPR-CPU-ORG/tg
+  cd tg
+  ./configure ;make
+  cd ..
+}
+install_rocks() {
+  ./.luarocks/bin/luarocks install luasocket
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install oauth
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install redis-lua
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install lua-cjson
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install fakeredis
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install xml
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install feedparser
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
+
+  ./.luarocks/bin/luarocks install serpent
+  RET=$?; if [ $RET -ne 0 ];
+    then echo "Error. Exiting."; exit $RET;
+  fi
 }
 
 install() {
+  in_tg
   git pull
-  git submodule update --init --recursive
-  patch -i "patches/disable-python-and-libjansson.patch" -p 0 --batch --forward
+  git submodule update —init —recursive
+  patch -i "patches/disable-python-and-libjansson.patch" -p 0 —batch —forward
   RET=$?;
 
   cd tg
@@ -24,6 +98,8 @@ install() {
     echo "Error. Exiting."; exit $RET;
   fi
   cd ..
+  install_luarocks
+  install_rocks
 }
 
 if [ "$1" = "install" ]; then
@@ -42,6 +118,9 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/bot.lua -l 1 -E $@
+  while true; do
+   rm -r ../.telegram-cli/state
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./source/bot/f80bot.lua -l 1 -E $@
+   sleep 3
+  done
 fi
